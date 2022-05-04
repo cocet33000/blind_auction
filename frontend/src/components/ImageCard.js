9573586af41a3a5d70e9d379ff55a7186028fed0
cocet33000 <cocet33000@gmail.com>
2022-05-04 17:03:28 +0900

    fix

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -2,14 +2,8 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
-import { width } from "@mui/system";
 
 export default function ImageCard(props) {
   const theme = useTheme();

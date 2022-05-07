006ede2d940485df9404e903c9ee66a4a7350091
cocet33000 <cocet33000@gmail.com>
2022-05-07 10:41:18 +0900

    🔨  register_bidの引数名を変更

--- backend/main/UseCase/register_bid.py
+++ backend/main/UseCase/register_bid.py
@@ -5,15 +5,15 @@ from DomainModel import Bid
 from Repository import BidRepository
 
 
-def register_bid(bided_user_id: int, bid_item_id: int, bid_price: int) -> dict:
+def register_bid(user_id: int, item_id: int, price: int) -> dict:
     bid = Bid(
-        bided_user_id=bided_user_id,
-        bid_item_id=bid_item_id,
+        bided_user_id=user_id,
+        bid_item_id=item_id,
         bided_at=datetime.datetime.now(),
-        price=bid_price,
+        price=price,
     )
     return BidRepository.save(bid)
 
 
 if __name__ == "__main__":
-    print(json.dumps(register_bid(bided_user_id=1, bid_item_id=9, bid_price=10000)))
+    print(json.dumps(register_bid(user_id=1, item_id=9, price=10000)))
---
+++ backend/main/test.py
@@ -0,0 +1,14 @@
+from UseCase import get_items
+from UseCase import register_item
+from UseCase import register_bid
+
+print(get_items())
+print(register_bid(user_id=1, item_id=9, price=10000))
+print(
+    register_item(
+        name="hoge",
+        image_src="http://test",
+        description="hoge hoge",
+        start_price=100,
+    )
+)

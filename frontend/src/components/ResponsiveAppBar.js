726683d7fa2e47dd62091ca0fcc0861f868d0a43
cocet33000 <cocet33000@gmail.com>
2022-05-07 12:01:13 +0900

    ✨  入札機能を仮実装

--- frontend/src/components/ResponsiveAppBar.js
+++ frontend/src/components/ResponsiveAppBar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import * as React from 'react';
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -13,13 +13,10 @@ import MenuItem from "@mui/material/MenuItem";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LoginIcon from "@mui/icons-material/Login";
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-
-
-
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -34,23 +31,20 @@ const darkTheme = createTheme({
   },
 });
 
-
 const ResponsiveAppBar = () => {
   let navigate = useNavigate();
-  const { authStatus } = useAuthenticator(context => [context.authStatus]);
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
   const { user, signOut } = useAuthenticator((context) => [context.user]);
   const [isNotification, setNotification] = useState("");
 
   useEffect(() => {
     setNotification(true);
-    console.log("auth status is changed!")
+    console.log("auth status is changed!");
   }, [authStatus]);
-
 
   const handleClose = (event, reason) => {
     setNotification(false);
   };
-
 
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
@@ -91,7 +85,10 @@ const ResponsiveAppBar = () => {
 
   const authenticatedMenu = [
     {
-      text: authStatus !== 'authenticated' ? ("") : ("You are signed in as " + user.username),
+      text:
+        authStatus !== "authenticated"
+          ? ""
+          : "You are signed in as " + user.username,
       onClick: handleLogout,
     },
   ];
@@ -109,7 +106,9 @@ const ResponsiveAppBar = () => {
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <Typography
-              onClick={() => { navigate("/") }}
+              onClick={() => {
+                navigate("/");
+              }}
               variant="h6"
               noWrap
               component="div"
@@ -162,7 +161,9 @@ const ResponsiveAppBar = () => {
               </Menu>
             </Box>
             <Typography
-              onClick={() => { navigate("/") }}
+              onClick={() => {
+                navigate("/");
+              }}
               variant="h6"
               noWrap
               component="div"
@@ -186,7 +187,7 @@ const ResponsiveAppBar = () => {
 
             <Box sx={{ flexGrow: 0 }}>
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {authStatus !== 'authenticated' ? (
+                {authStatus !== "authenticated" ? (
                   <LoginIcon fontSize="large" />
                 ) : (
                   <AccountCircleIcon fontSize="large" />
@@ -208,7 +209,10 @@ const ResponsiveAppBar = () => {
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
               >
-                {(authStatus !== 'authenticated' ? notAuthenticatedMenu : authenticatedMenu).map((setting) => (
+                {(authStatus !== "authenticated"
+                  ? notAuthenticatedMenu
+                  : authenticatedMenu
+                ).map((setting) => (
                   <MenuItem key={setting.text} onClick={handleCloseUserMenu}>
                     <Typography textAlign="center" onClick={setting.onClick}>
                       {setting.text}
@@ -221,13 +225,15 @@ const ResponsiveAppBar = () => {
         </Container>
       </AppBar>
       <Snackbar
-        open={isNotification && authStatus === 'authenticated'}
+        open={isNotification && authStatus === "authenticated"}
         autoHideDuration={6000}
         onClose={handleClose}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
       >
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          {authStatus !== 'authenticated' ? "" : "You are signed in as " + user.username}
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          {authStatus !== "authenticated"
+            ? ""
+            : "You are signed in as " + user.username}
         </Alert>
       </Snackbar>
     </ThemeProvider>

52551c5df7a014b17cd03ae0be3573e093382a2e
cocet33000 <cocet33000@gmail.com>
2022-05-14 22:32:16 +0900

    モックを利用したテストを追加

--- backend/main/Infrastructure/__init__.py
+++ backend/main/Infrastructure/__init__.py
@@ -1,20 +1,11 @@
 from .BidRepository import BidRepository
 from .ItemRepository import ItemRepository
-
 from .BidRepositoryImpl import BidRepositoryImpl as BidRepositoryImplDynamoDB
 from .ItemRepositoryImpl import ItemRepositoryImpl as ItemRepositoryImplDynamoDB
 
-from injector import Injector, Module
-
-
-class DIModule(Module):
-    def configure(self, binder):
-        binder.bind(BidRepository, to=BidRepositoryImplDynamoDB)
-        binder.bind(ItemRepository, to=ItemRepositoryImplDynamoDB)
-
-
-injector = Injector([DIModule()])
-BidRepositoryImpl: BidRepository = injector.get(BidRepository)
-ItemRepositoryImpl: ItemRepository = injector.get(ItemRepository)
-
-__all__ = ["BidRepositoryImpl", "ItemRepositoryImpl"]
+__all__ = [
+    "BidRepository",
+    "ItemRepository",
+    "BidRepositoryImplDynamoDB",
+    "ItemRepositoryImplDynamoDB",
+]
--- backend/main/test.py
+++ backend/main/test.py
@@ -1,16 +1,34 @@
-from UseCase import get_items
-from UseCase import register_item
-from UseCase import register_bid
-from UseCase import get_bids_by_user
+from UseCase import ItemUseCase
+from UseCase import BidUseCase
 
-print(get_items())
-print(register_bid(user_name="fuga", item_id=4, price=10000))
-print(
-    register_item(
-        name="hoge",
-        image_src="http://test",
-        description="hoge hoge",
-        start_price=100,
+from Infrastructure import BidRepository
+from Infrastructure import ItemRepository
+
+from Infrastructure import BidRepositoryImplDynamoDB
+from Infrastructure import ItemRepositoryImplDynamoDB
+
+from injector import Injector, Module, singleton, inject
+
+
+@singleton
+class DIModule(Module):
+    def configure(self, binder):
+        binder.bind(BidRepository, to=BidRepositoryImplDynamoDB)
+        binder.bind(ItemRepository, to=ItemRepositoryImplDynamoDB)
+
+
+if __name__ == "__main__":
+    injector = Injector([DIModule()])
+    item_usecase = injector.get(ItemUseCase)
+    bid_usecase = injector.get(BidUseCase)
+    print(item_usecase.get_items())
+    print(
+        item_usecase.register_item(
+            name="hoge",
+            image_src="http://test",
+            description="hoge hoge",
+            start_price=100,
+        )
     )
-)
-print(get_bids_by_user(user_name="hoge"))
+    print(bid_usecase.register_bid(user_name="fuga", item_id=4, price=10000))
+    print(bid_usecase.get_bids_by_user(user_name="hoge"))

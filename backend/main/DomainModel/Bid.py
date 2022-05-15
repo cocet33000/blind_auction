47d3debf46cf77c9a1ff82ae5a747e5341818397
江藤晃一 <eto@etoukouichinoMacBook-Pro.local>
2022-05-15 18:19:26 +0900

    コミットが行方不明になったのでreset

--- backend/main/DomainModel/Bid.py
+++ backend/main/DomainModel/Bid.py
@@ -10,12 +10,8 @@ class Bid:
     bided_at: datetime
 
     def __post_init__(self):
-        if not (isinstance(self.bided_user_name, str) and isinstance(self.bid_item_id, int) and isinstance(self.price, int) and isinstance(self.bided_at, datetime)):
+        if not isinstance(self.price, int):
             raise TypeError
-        if not (1 <= self.bid_item_id < 100000000):
-            raise ValueError
-        if not (0 <= self.price < 1000000):
-            raise ValueError
 
     def to_dict(self) -> dict:
         return {

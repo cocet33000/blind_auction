a177121667d11914b2c7c1b593324db579e34a29
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-05-16 22:19:49 +0900

    不要遷移の削除

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import A from "./A";
-import B from "./B";
 
 import Home from "./Home";
 import AppBar from "./AppBar.js";
@@ -15,8 +13,6 @@ function App() {
         <AppBar />
         <Container maxWidth="md">
           <Routes>
-            <Route path="/a" element={<A />} />
-            <Route path="/b" element={<B />} />
             <Route path="/auth" element={<CognitoAuthenticator />} />
             <Route path="/" element={<Home />} />
           </Routes>

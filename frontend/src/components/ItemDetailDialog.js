41c42c452e44554b37362559d49e422c5f9f9b02
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-05-19 23:17:46 +0900

    モーダルに入札ボタンを追加

--- frontend/src/components/ItemDetailDialog.js
+++ frontend/src/components/ItemDetailDialog.js
@@ -8,34 +8,80 @@ import DialogContentText from "@mui/material/DialogContentText";
 import { TextField } from "@mui/material";
 import { InputAdornment } from "@mui/material";
 import { Input } from "@mui/material";
+import { Stack, Box } from "@mui/material";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import axios from "axios";
 
 export default function ItemDetailDialog(props) {
+  const [price, setPrice] = React.useState("");
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+  const { user } = useAuthenticator((context) => [context.user]);
+
+  const bid = () => {
+    const data = {
+      user_name: user.username,
+      item_id: props.item_id,
+      price: Number(price),
+    };
+    console.log(data);
+
+    axios
+      .post("https://api.blind-auction.com/dev/bids", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log("ERROR!! occurred in Backend.");
+      });
+
+    props.handleClose();
+  };
+
   return (
     <main>
       {props.item ? (
-        <Dialog open={props.isOpen}>
+        <Dialog open={props.isOpen} onClose={props.handleClose}>
           <DialogTitle>{props.item.name}</DialogTitle>
           <DialogContent>
             <img
               style={{ maxWidth: "100%", maxHeight: "calc(100vh - 64px)" }}
               src={props.item.image_src}
             />
-            <DialogContentText sx={{ fontSize: 24 }}>
-              {props.item.description}
-            </DialogContentText>
-            <Input
-              fullWidth={true}
-              placeholder={"初期価格：" + props.item.start_price}
-              id="standard-adornment-amount"
-              type="number"
-              variant="standard"
-              startAdornment={
-                <InputAdornment position="start">¥</InputAdornment>
-              }
-            />
-            <DialogActions>
-              <Button onClick={props.handleClose}>Cancel</Button>
-            </DialogActions>
+            <Box
+              sx={{
+                height: 150,
+              }}
+            >
+              <DialogContentText sx={{ fontSize: 24 }}>
+                {props.item.description}
+              </DialogContentText>
+            </Box>
+            <Stack
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Input
+                placeholder={"初期価格：" + props.item.start_price}
+                id="standard-adornment-amount"
+                type="number"
+                variant="standard"
+                value={price}
+                onChange={(event) => setPrice(event.target.value)}
+                startAdornment={
+                  <InputAdornment position="start">¥</InputAdornment>
+                }
+              />
+
+              <Button
+                variant="contained"
+                onClick={bid}
+                disabled={authStatus === "authenticated" ? false : true}
+              >
+                {authStatus === "authenticated" ? "BID" : "NEED SignIn"}
+              </Button>
+            </Stack>
           </DialogContent>
         </Dialog>
       ) : (

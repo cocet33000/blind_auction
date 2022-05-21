a32af8215b0a4d1833b7bca1e6db750dc1ed5963
江藤晃一 <eto@etoukouichinoMacBook-Pro.local>
2022-05-21 11:17:51 +0900

    #26 バリデーションを追加

--- backend/main/DomainModel/Item.py
+++ backend/main/DomainModel/Item.py
@@ -10,5 +10,15 @@ class Item:
     start_price: int
     bid_num: int
 
+    def __post_init__(self):
+        if not (isinstance(self.id, int) and isinstance(self.name, str) and isinstance(self.image_src, str) and isinstance(self.description, str) and isinstance(self.start_price, int) and isinstance(self.bid_num, int)):
+            raise TypeError
+        if not (1 <= self.id < 100000000):
+            raise ValueError
+        if not (0 <= self.start_price < 1000000):
+            raise ValueError
+        if not (0 <= self.bid_num < 100000000):
+            raise ValueError
+
     def to_dict(self) -> dict:
         return asdict(self)

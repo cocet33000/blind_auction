a6f1658bd0eb882a74d78110858420f9c850df0d
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-06-04 10:55:32 +0900

    階層変更

--- backend/test/UseCaseTest/BidUseCaseTest/test_register_bid.py
+++ backend/test/UseCaseTest/BidUseCaseTest/test_register_bid.py
@@ -8,19 +8,21 @@ import sys
 
 from injector import Injector, Module, singleton, inject
 
-sys.path.append(str(Path(__file__).parent.parent.parent / "main"))
+sys.path.append(str(Path(__file__).parent.parent.parent.parent / "main"))
 from Infrastructure import BidRepository, ItemRepository
 import DomainModel
 from UseCase import BidUseCase
 
 item_repository_mock = Mock(spec=ItemRepository)
 bid_repository_mock = Mock(spec=BidRepository)
-bid_repository_mock.getByUserName.return_value = [DomainModel.Bid(
-    bided_user_name = "hoge",
-    bid_item_id = 2,
-    price = 1000,
-    bided_at= datetime.datetime.now(),
-)]
+bid_repository_mock.getByUserName.return_value = [
+    DomainModel.Bid(
+        bided_user_name="hoge",
+        bid_item_id=2,
+        price=1000,
+        bided_at=datetime.datetime.now(),
+    )
+]
 
 
 @singleton
@@ -31,12 +33,12 @@ class DIModule(Module):
 
 
 class TestRegisterBid(unittest.TestCase):
-    def test_ユーザーは同じ商品に一度しか入札できない(self):
+    def test_既に入札済みのユーザーの場合は例外を投げる(self):
         injector = Injector([DIModule()])
         bid_usecase = injector.get(BidUseCase)
 
         test_user_name = "hoge"
-        test_item_id = 1
+        test_item_id = 2
         test_price = 1000
 
         # 既に入札済みのユーザーの場合は例外を投げる

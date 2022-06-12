3ab58fe79e9b091fde5e29a16f48e258a2de918a
cocet33000 <cocet33000@gmail.com>
2022-06-12 19:46:41 +0900

    ✨ presentation層のテストを追加

---
+++ backend/test/presentation/lambda_handler/test_handler.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+import unittest
+
+from main.presentation.lambda_handler import handler
+
+from mock import Mock
+
+from main.usecase import ItemUseCase
+from main.usecase import BidUseCase
+
+from main.domain.item import Item
+from main.domain.value_object import Price
+
+items = [
+    Item(
+        id="1",
+        name="hoge",
+        image_src="test.png",
+        description="hoge",
+        start_price=Price(100),
+        bid_num=0,
+    )
+]
+
+item_usecase_mock = Mock(spec=ItemUseCase)
+item_usecase_mock.get_items.return_value = {"items": [item.to_dict() for item in items]}
+bid_usecase_mock = Mock(spec=BidUseCase)
+
+
+class TestBidModel(unittest.TestCase):
+    def test_(self):
+        # TODO: リクエスト内容を別ファイルで用意する
+        event = {
+            "pathParameters": {"proxy": "items"},
+            "requestContext": {"http": {"method": "GET"}},
+        }
+        response = handler(event, "", item_usecase_mock, bid_usecase_mock)
+        self.assertEqual(
+            '{"items": [{"id": "1", "name": "hoge", "image_src": "test.png", "description": "hoge", "start_price": 100, "bid_num": 0}]}',
+            response.get("body"),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()

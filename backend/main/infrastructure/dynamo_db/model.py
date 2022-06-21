5df6e757175cca96fe8339e04a4c83f51b47d830
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-06-21 23:56:33 +0900

    モデル変更（テーブル定義変更の為）

--- backend/main/infrastructure/dynamo_db/model.py
+++ backend/main/infrastructure/dynamo_db/model.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 from pynamodb.attributes import NumberAttribute
-from pynamodb.attributes import ListAttribute
-from pynamodb.attributes import MapAttribute
+from pynamodb.indexes import LocalSecondaryIndex, AllProjection
 from pynamodb.attributes import UTCDateTimeAttribute
 
 from main.domain.bid import Bid as DomainModelBid
@@ -13,18 +12,37 @@ from main.domain.item import Item as DomainModelItem
 from main.domain.value_object import Price
 
 
-class Bid(MapAttribute):
+class Bid(Model):
+    class Meta:
+        load_dotenv()
+        if os.environ.get("MODE") == "local":
+            host = "http://localhost:8000"
+        table_name = os.environ.get("AWS_DYNAMO_DB_ITEMS_TABLE_NAME")
+        region = os.environ.get("AWS_REGION")
+
+    id = UnicodeAttribute(hash_key=True)
+    range_key = UnicodeAttribute(range_key=True)
     price = NumberAttribute(null=False)
     bided_user_name = UnicodeAttribute(null=False)
     bided_at = UTCDateTimeAttribute(null=False)
 
     def to_model(self, item_id: int) -> DomainModelBid:
         return DomainModelBid(
+            id=self.range_key,
             bided_user_name=self.bided_user_name,
             bided_at=self.bided_at,
-            bid_item_id=item_id,
+            bid_item_id=self.id,
             price=Price(self.price),
         )
+
+
+class GetAllItemsIndex(LocalSecondaryIndex):
+    class Meta:
+        index_name = "name-index"
+        projection = AllProjection()
+
+    id = UnicodeAttribute(hash_key=True)
+    name = UnicodeAttribute(range_key=True)
 
 
 class Item(Model):
@@ -36,11 +54,12 @@ class Item(Model):
         region = os.environ.get("AWS_REGION")
 
     id = UnicodeAttribute(hash_key=True)
+    range_key = UnicodeAttribute(range_key=True)
     name = UnicodeAttribute(null=False)
     image_src = UnicodeAttribute(null=False)
     description = UnicodeAttribute(null=False)
     start_price = NumberAttribute(null=False)
-    bids = ListAttribute(of=Bid, null=True)
+    getAllItemsIndex = GetAllItemsIndex()
 
     def to_model(self) -> DomainModelItem:
         return DomainModelItem(
@@ -49,5 +68,5 @@ class Item(Model):
             image_src=self.image_src,
             description=self.description,
             start_price=Price(self.start_price),
-            bid_num=len(self.bids) if isinstance(self.bids, list) else 0,
+            bid_num=0,  # あとで修正する
         )

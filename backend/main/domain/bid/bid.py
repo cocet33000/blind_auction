36f43245eb64d1c702703db1652f4a84400c8fda
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-06-21 23:54:26 +0900

    Bidモデルに属性追加（テーブル定義変更のため）

--- backend/main/domain/bid/bid.py
+++ backend/main/domain/bid/bid.py
@@ -18,6 +18,7 @@ class Bid:
 
     def to_dict(self) -> dict:
         return {
+            "id": self.id,
             "bided_user_name": self.bided_user_name,
             "bid_item_id": self.bid_item_id,
             "price": self.price,

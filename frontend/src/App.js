7c968bc98fd800b6bf4eb20bcc4c0ba4f52eeef1
cocet33000 <cocet33000@gmail.com>
2022-07-03 19:23:19 +0900

    🔨  Linterに沿って修正

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import "./App.css";
+import './App.css';
 
-import Home from "./Home";
-import AppBar from "./AppBar.js";
-import CognitoAuthenticator from "./components/CognitoAuthenticator.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container } from "@mui/material";
+import Home from './Home.js';
+import AppBar from './AppBar.js';
+import CognitoAuthenticator from './components/CognitoAuthenticator.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <AppBar />
-        <Container maxWidth="md">
-          <Routes>
-            <Route path="/auth" element={<CognitoAuthenticator />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Container>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<AppBar />
+				<Container maxWidth="md">
+					<Routes>
+						<Route path="/auth" element={<CognitoAuthenticator />} />
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</Container>
+			</Router>
+		</div>
+	);
 }
 
 export default App;

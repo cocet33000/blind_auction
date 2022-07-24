f08817367372ac51a7166f2d8277ea6a3bb4e0da
cocet33000 <cocet33000@gmail.com>
2022-07-24 10:14:29 +0900

    🔨 ファクトリメソッドを実装

--- backend/test/domain/auction/test_auction.py
+++ backend/test/domain/auction/test_auction.py
@@ -1,13 +1,17 @@
 import datetime
+import pytest
+import uuid
 
 from main.domain.auction import Auction
 from main.domain.auction import Status
+from main.domain.shared.errors.errors import ProhibitedGenerationError
 
 
-def test_正常系_auctionインスタンスを生成():
+def test_外部からのauctionの生成はNG():
+    ID = str(uuid.uuid4())
     NAME = "test_auction"
     STATUS = Status.OPEN
     START_DATETIME = datetime.datetime(2022, 10, 1, 0, 0, 0)
     END_DATETIME = datetime.datetime(2022, 10, 31, 0, 0, 0)
-    auction = Auction(NAME, STATUS, START_DATETIME, END_DATETIME)
-    assert auction is not None
+    with pytest.raises(ProhibitedGenerationError):
+        Auction(ID, NAME, STATUS, START_DATETIME, END_DATETIME)

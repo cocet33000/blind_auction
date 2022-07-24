11fcf5ed63e378a2f57d750486e7c34d23e15d06
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-07-24 11:02:35 +0900

    バグ修正

--- frontend/src/components/CountDown.js
+++ frontend/src/components/CountDown.js
@@ -127,12 +127,26 @@ const CountdownTimer = ({ targetDate }) => {
 		return <ExpiredNotice />;
 	} else {
 		return (
-			<ShowCounter
-				days={days}
-				hours={hours}
-				minutes={minutes}
-				seconds={seconds}
-			/>
+			<main>
+				<Grid sx={{ flexGrow: 1 }} container spacing={1}>
+					<Grid item xs={12}>
+						<Grid container justifyContent="center" spacing={3}>
+							<Grid key={'day'} item>
+								<DateTimeDisplay value={days} type={0} />
+							</Grid>
+							<Grid key={'hours'} item>
+								<DateTimeDisplay value={hours} type={1} />
+							</Grid>
+							<Grid key={'minutes'} item>
+								<DateTimeDisplay value={minutes} type={2} />
+							</Grid>
+							<Grid key={'seconds'} item>
+								<DateTimeDisplay value={seconds} type={3} />
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
+			</main>
 		);
 	}
 };

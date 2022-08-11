69482388538e098fb64e52ad2d7da4fbbf44f590
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-08-11 22:56:26 +0900

    余白調整

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,35 @@ import Home from './Home.js';
 import AppBar from './components/AppBar.js';
 import CognitoAuthenticator from './components/CognitoAuthenticator.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Container } from '@mui/material';
+// import { styled } from '@mui/system';
+import Box from '@mui/material/Box';
+// import { Container } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+	// const StyledBox = styled(Box)(({ theme }) => ({
+	// 	...theme.mixins.toolbar,
+	// 	border: '1px solid'
+	// }));
+
+	const theme = createTheme();
 	return (
-		<div>
+		<ThemeProvider theme={theme}>
 			<Router>
 				<AppBar />
-				<Container maxWidth="md">
-					<Routes>
-						<Route path="/auth" element={<CognitoAuthenticator />} />
-						<Route path="/" element={<Home />} />
-					</Routes>
-				</Container>
+				{/* appbarと重なる為、余白用のBoxをおく */}
+				<Box
+					sx={{
+						width: 300,
+						height: theme.mixins.toolbar
+					}}
+				/>
+				<Routes>
+					<Route path="/auth" element={<CognitoAuthenticator />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
 			</Router>
-		</div>
+		</ThemeProvider>
 	);
 }
 

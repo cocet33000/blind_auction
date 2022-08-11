8fb5d4b3c98b407fb4c68f66b8ce44af8f6d90a1
Ao Miyazawa <65105110+AoMiyazawa@users.noreply.github.com>
2022-08-12 00:29:47 +0900

    Revert "フロントエンド微修正"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,44 +4,21 @@ import Home from './Home.js';
 import AppBar from './components/AppBar.js';
 import CognitoAuthenticator from './components/CognitoAuthenticator.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { styled } from '@mui/system';
-import Box from '@mui/material/Box';
-// import { Container } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Container } from '@mui/material';
 
 function App() {
-	// const StyledBox = styled(Box)(({ theme }) => ({
-	// 	...theme.mixins.toolbar,
-	// 	border: '1px solid'
-	// }));
-
-	const theme = createTheme({
-		palette: {
-			primary: {
-				main: '#26262d'
-			},
-			card: {
-				background: '#f5f5f5'
-			}
-		}
-	});
 	return (
-		<ThemeProvider theme={theme}>
+		<div>
 			<Router>
 				<AppBar />
-				{/* appbarと重なる為、余白用のBoxをおく */}
-				<Box
-					sx={{
-						width: 300,
-						height: theme.mixins.toolbar
-					}}
-				/>
-				<Routes>
-					<Route path="/auth" element={<CognitoAuthenticator />} />
-					<Route path="/" element={<Home />} />
-				</Routes>
+				<Container maxWidth="md">
+					<Routes>
+						<Route path="/auth" element={<CognitoAuthenticator />} />
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</Container>
 			</Router>
-		</ThemeProvider>
+		</div>
 	);
 }
 

768ed64ddd7d177aac4b3647f073a29d42a915d0
cocet33000 <cocet33000@gmail.com>
2022-08-13 13:23:44 +0900

    🔨  コメントアウトを元に戻す

--- backend/test/infrastructure/item_repository/test_bid_num_increment.py
+++ backend/test/infrastructure/item_repository/test_bid_num_increment.py
@@ -1,7 +1,23 @@
+import uuid
+
 from main.infrastructure.item_repository_impl import ItemRepositoryImpl
 
-# ローカルでは、コンテナで立てたDynamoDBでテスト
-# CI/CDではテスト対象がないためとりあずコメントアウトしておく
-# def test_正常系():
-#     item_id = "f99dd977-7635-456b-bdb9-1b4da09f9696"
-#     ItemRepositoryImpl.bidNumIncrement(item_id)
+from main.domain.value_object import Price
+from main.domain.item import Item, Status
+
+
+def test_正常系():
+    item_id = str(uuid.uuid4())
+
+    item = Item.reconstruct(
+        id=item_id,
+        status=Status.BEFORE_AUCTION,
+        name="hoge",
+        image_src="hoge",
+        description="hoge",
+        start_price=Price(100),
+        bid_num=0,
+    )
+    ItemRepositoryImpl.save(item)
+
+    ItemRepositoryImpl.bidNumIncrement(item_id)

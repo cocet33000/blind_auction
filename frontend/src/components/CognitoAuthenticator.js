1f6e5163473ea0b0b525f48974acc6812a74aa46
cocet33000 <cocet33000@gmail.com>
2022-08-20 11:41:29 +0900

    ✨ 認証画面のカラーを変更

--- frontend/src/components/CognitoAuthenticator.js
+++ frontend/src/components/CognitoAuthenticator.js
@@ -1,14 +1,15 @@
+import {
+	Authenticator,
+	ThemeProvider as TP,
+	useTheme
+} from '@aws-amplify/ui-react';
+
+import { useTheme as muiUseTheme } from '@mui/material/styles';
+
 import { Navigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import {
-	Image,
-	View,
-	useTheme,
-	Text,
-	translations
-} from '@aws-amplify/ui-react';
+import { Image, View, Text, translations } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { I18n } from 'aws-amplify';
 // I18n.putVocabularies(vocabularies);
@@ -23,6 +24,71 @@ Amplify.configure({
 });
 
 export default function CognitoAuthenticator() {
+	const muiTheme = muiUseTheme();
+	const theme = {
+		name: 'Auth Example Theme',
+		tokens: {
+			colors: {
+				background: {
+					primary: {
+						value: muiTheme.palette.authenticator.background.primary
+					},
+					secondary: {
+						value: muiTheme.palette.authenticator.background.secondary
+					}
+				},
+				font: {
+					interactive: {
+						value: muiTheme.palette.authenticator.font.interactive
+					}
+				}
+			},
+			components: {
+				tabs: {
+					item: {
+						_focus: {
+							color: {
+								value: muiTheme.palette.authenticator.font.focus
+							}
+						},
+						_hover: {
+							color: {
+								value: muiTheme.palette.authenticator.font.hover
+							}
+						},
+						_active: {
+							color: {
+								value: muiTheme.palette.authenticator.font.active
+							}
+						}
+					}
+				},
+				button: {
+					fontWeight: { value: '{fontWeights.extrabold}' },
+					primary: {
+						backgroundColor: {
+							value: muiTheme.palette.authenticator.components.button.background
+						},
+						_hover: {
+							backgroundColor: {
+								value: muiTheme.palette.authenticator.components.button.hover
+							}
+						},
+						_focus: {
+							backgroundColor: {
+								value: muiTheme.palette.authenticator.components.button.focus
+							}
+						},
+						_active: {
+							backgroundColor: {
+								value: muiTheme.palette.authenticator.components.button.active
+							}
+						}
+					}
+				}
+			}
+		}
+	};
 	const formFields = {
 		signIn: {
 			username: {
@@ -79,13 +145,15 @@ export default function CognitoAuthenticator() {
 					alignItems: 'center'
 				}}
 			>
-				<Authenticator
-					signUpAttributes={['email']}
-					components={components}
-					formFields={formFields}
-				>
-					{() => <Navigate to="/" />}
-				</Authenticator>
+				<TP theme={theme}>
+					<Authenticator
+						signUpAttributes={['email']}
+						components={components}
+						formFields={formFields}
+					>
+						{() => <Navigate to="/" />}
+					</Authenticator>
+				</TP>
 			</Box>
 		</main>
 	);

5a68df1ca5a0dfe04d24cdcb4d892d522e50ce27
cocet33000 <cocet33000@gmail.com>
2022-08-27 10:26:12 +0900

    ✨  オークションイベント追加

--- backend/main/domain/auction/__init__.py
+++ backend/main/domain/auction/__init__.py
@@ -2,5 +2,6 @@ from .auction import Auction
 from .auction import Status
 from .auction_factory import AuctionFactory
 from .auction_repository import AuctionRepository
+from .auction_event import AuctionEvent
 
-__all__ = ["Auction", "Status", "AuctionFactory", "AuctionRepository"]
+__all__ = ["Auction", "Status", "AuctionFactory", "AuctionRepository", "AuctionEvent"]

8559f264b2273a952ae89efb15c140ea65b04e71
Ao Myazawa <miyazawa.a2020@gmail.com>
2022-09-13 00:17:30 +0900

    PynamoDBのEventがEnumをsave出来ないため、文字列に変換

--- backend/main/infrastructure/event_publisher_impl.py
+++ backend/main/infrastructure/event_publisher_impl.py
@@ -12,7 +12,12 @@ class EventPublisherImpl(EventPublisher):
     def publish(event: Event) -> None:
         new_event = dynamo_db.Event(hash_key=str(uuid.uuid4()))
         new_event.name = event.event_name
-        new_event.details = event.event_details
+        # PynamoDBのEventがEnumをsave出来ないため、文字列に変換
+        new_event.details = {
+            "auction_id": event.event_details["auction_id"],
+            "name": event.event_details["name"],
+            "type": event.event_details["type"].value,
+        }
 
         try:
             new_event.save()

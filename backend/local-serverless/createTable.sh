a7eb0896eb1291af9432cbd494163453f3e79d67
Ao Myazawa <miyazawa.a2020@gmail.com>
2023-03-08 14:38:39 +0900

    chore:local-dynamo起動時にテーブル定義作成とサンプルレコードを追加

---
+++ backend/local-serverless/createTable.sh
@@ -0,0 +1,4 @@
+cd `dirname $0`
+aws dynamodb create-table --cli-input-json file://config/amtest-blind-auction-items.json --endpoint-url http://localhost:8000 --region ap-northeast-1
+aws dynamodb create-table --cli-input-json file://config/blind_auction_events.json --endpoint-url http://localhost:8000 --region ap-northeast-1
+aws dynamodb batch-write-item --request-items file://config/sample-items.json --endpoint-url http://localhost:8000 --region ap-northeast-1
